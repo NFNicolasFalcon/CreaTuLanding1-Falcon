bd3880e198c5379d6e28fe7dab1489dda264423d
NFNicolasFalcon <nicolasfalcon14@gmail.com>
2025-01-16 19:32:38 -0300

    CssXcomp

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'; 
+import './assets/components/NavbarComp.css';
+import './assets/components/WelcomeSectionComp.css';
 import Navbar from './assets/components/NavbarComp'; 
 import WelcomeSection from './assets/components/WelcomeSectionComp'; 
 
